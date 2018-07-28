043ae1aad4953090038d690862a69c05ee1490ae
sty <jsjtsty@163.com>
2018-07-29 01:26:26 +0800

    v1.3.1 Update

--- ZcNoPing/AboutDialog.cpp
+++ ZcNoPing/AboutDialog.cpp
@@ -2,6 +2,7 @@
 #include "AboutDialog.h"
 #include "resource.h"
 #include "MessageBoxOK.h"
+#include <ShellAPI.h>
 using namespace DuiLib;
 
 void AboutDialog::Notify(DuiLib::TNotifyUI & msg)
@@ -25,6 +26,7 @@ The author accepts no liability if it causes any damage to you or your\n\
 computer whatsoever.It's free, so don't hassle me about it.\n\
 Beware of bugs."), 700, 450);
 			MessageBoxOK::Show(m_hWnd, TEXT("DuiLib License"), TEXT("Copyright (c) 2010-2011, duilib develop team(https://github.com/duilib/duilib).All rights reserved.\n\nRedistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met.\n\nRedistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n\nRedistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n\nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."), 700, 750);
+			ShellExecute(GetHWND(), TEXT("open"), TEXT("http://www.gnu.org/licenses/gpl-3.0.en.html"), NULL, NULL, SW_SHOWMAXIMIZED);
 		}
 	}
 	__super::Notify(msg);
